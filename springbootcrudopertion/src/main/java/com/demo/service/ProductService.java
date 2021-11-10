e2851be8fee926f471f973015a5992a05798966e
unknown <awsdev747@gmail.com>
2021-11-10 14:56:01 +0530

    spring product demo

--- springbootcrudopertion/src/main/java/com/demo/service/ProductService.java
+++ springbootcrudopertion/src/main/java/com/demo/service/ProductService.java
@@ -9,26 +9,26 @@ import com.demo.pojo.Product;
 
 public interface ProductService{
 	
-	Product CreatProductDetails(Product product);
+	Product CreatProductDetails(Product product); ///done
 	
 	// add for product
 		Product addProductDetails(Product product);
 
 		// list of all products
 
-		ArrayList<Product> ListOfProducts(Product product);
+		ArrayList<Product> ListOfProducts(Product product);  //pending
 
 		// Delete of Product
 
-		void deleteProductById(long id);
+		void deleteProductById(long id); //pending
 
 		// view of particular Product by id
 
-	     Optional<Product> getProductById(long id);
+	     Optional<Product> getProductById(long id);  //pending
 
 		// update of particular Product by id
 
-		void updateOfProductById(long id);
+		void updateOfProductById(long id); //pending..
 	
 }
 	
