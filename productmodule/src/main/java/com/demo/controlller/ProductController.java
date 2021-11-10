8ed48824b1e46bcaa6fd7e6d3d6a2c2c2810777f
unknown <awsdev747@gmail.com>
2021-11-10 23:04:43 +0530

    added crud funcationality edited

--- productmodule/src/main/java/com/demo/controlller/ProductController.java
+++ productmodule/src/main/java/com/demo/controlller/ProductController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,7 +56,7 @@ public class ProductController {
 	}
 
 	// api for delete product by id
-	@GetMapping("/productdeletedbyid/{id}")
+	@DeleteMapping("/productdeletedbyid/{id}")
 	public Map<String,Boolean> deleteById(@PathVariable(value = "id") Long id) {
 	      Map<String,Boolean> response = new HashMap<>();
 		
