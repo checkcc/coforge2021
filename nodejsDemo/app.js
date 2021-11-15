1cc82f5f04d031d04ff73589a97a3f893f930f7b
unknown <awsdev747@gmail.com>
2021-11-15 10:24:40 +0530

    node demo

--- nodejsDemo/app.js
+++ nodejsDemo/app.js
@@ -1,15 +1,18 @@
-var httpdemo = require("http");//dependencies
 
-httpdemo.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World...raju\n');
-   console.log("**********"+response)
-}).listen(1000);
 
-// Console will print the message
-console.log('Server running at http://localhost:1000/');
+
+var http = require('http');   // is the  the dependency need to insta
+
+var express = require('express')
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+
+
+  res.end('Hello World!grfgfgfg');
+
+console.log("server started running on port 12345")
+
+}).listen(12345);
